b9ca1d49dff5b208a7109986879348525a3a7645
corwinn <corwinn@users.noreply.github.com>
2023-07-11 21:20:53 +0300

    test: "bsa" archive: filter ".nif" files only

--- test.cpp
+++ test.cpp
@@ -297,9 +297,17 @@ int parse_nif_bsa_archive(FFD_NS::FFD & ffd, FFD_NS::Stream & bsa)
     flist_ptr = flist.operator BsaFile * ();
     // the last token is ""
     for (int i = 0; i < fnames.Count ()-1; i++, flist_ptr++) {
+        { //TODO if (FileName.Extension.ToLower () != "nif") continue;
+            auto fc = fnames[i].Split ('.');
+            if (fc.Count () <= 0) continue; // fn w/o an ext.
+            if (fc[fc.Count ()-1].Length () != 3) continue; // can't be "nif"
+            auto fn_ext = fc[fc.Count ()-1].AsZStr ();
+            if (fn_ext[0] != 'n' && fn_ext[0] != 'N') continue;
+            if (fn_ext[1] != 'i' && fn_ext[1] != 'I') continue;
+            if (fn_ext[2] != 'f' && fn_ext[2] != 'f') continue;
+        }
         flist_ptr->file_name = fnames[i];
-
-        flist_ptr->DbgPrint (); // continue;
+            flist_ptr->DbgPrint (); // continue;
         bsa.Reset ().Seek (flist_ptr->bsa_entry.dofs);
         u32 isize = flist_ptr->bsa_entry.size;
         if (isize & (1u<<30)) {
